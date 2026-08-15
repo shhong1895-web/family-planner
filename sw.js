5f80e42a72209ef8af48b59c1c0c86a8313c77b7
shhong1895-web <shhong1895@gmail.com>
2026-08-15 23:37:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'family-planner-build044-v1';
+const CACHE = 'family-planner-build045-v1';
 const APP_SHELL = './index.html';
 self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE).then(cache => cache.add(APP_SHELL))); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
